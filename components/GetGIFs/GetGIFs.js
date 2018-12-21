f9302a5c9c35adf7a50ff0cc462021f517633326
Pedro Hernandez <pherned@gmail.com>
2018-12-21 17:15:27 -0500

    basic random gif rendering

--- components/GetGIFs/GetGIFs.js
+++ components/GetGIFs/GetGIFs.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import { View, Image } from 'react-native';
 
- export default class GetGIFs extends Component {
+export default class GetGIFs extends Component {
+    constructor() {
+        super();
+        this.state = {
+            gifURL: './img/conyztyxb0bqbspqgsmg.gif',
+        }
+    }
+
+    componentDidMount = async () => {
+        const giphyAPI = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=2mXx0bqPcKrGh9aUaRWBC0q6wpa734gR&tag=christmas`);
+        const giphyData = await giphyAPI.json();
+
+        this.setState({
+            gifURL: giphyData.data.images.original.url,
+        })
+
+        console.log(this.state.gifURL);
+    }
 
     render() {
         return (
             <View>
                 <Image
-                    style={{ width: 300 }}
-                    source={ require('./img/conyztyxb0bqbspqgsmg.gif') }
+                    style={{ width: 300, height: 200 }}
+                    // source={ require('./img/conyztyxb0bqbspqgsmg.gif')}
+                    source={{ uri: `${this.state.gifURL}` }}
                 />
             </View>
         );
